6aa7b81fe02c8889322894ba001e95e3894270f7
hnmt2022 <chou0413bird@yahoo.co.jp>
2024-04-05 10:06:37 +0900

    Rubocopの実行、コメントアウトの消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
@@ -12,14 +11,11 @@ class User < ApplicationRecord
   validates :kana_first_name, presence: true
   validates :birthday, presence: true
 
-  validates_format_of :last_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"
-  validates_format_of :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"
-  validates_format_of :kana_last_name, with: /\A[ァ-ヶー]+\z/, message: "kana is invalid. Input full-width katakana characters"
-  validates_format_of :kana_first_name, with: /\A[ァ-ヶー]+\z/, message: "kana is invalid. Input full-width katakana characters"
+  validates_format_of :last_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'
+  validates_format_of :kana_last_name, with: /\A[ァ-ヶー]+\z/, message: 'kana is invalid. Input full-width katakana characters'
+  validates_format_of :kana_first_name, with: /\A[ァ-ヶー]+\z/, message: 'kana is invalid. Input full-width katakana characters'
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-
-
-
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end

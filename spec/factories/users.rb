c8f97949e0bd2cb347083e5ceaafd08ea911ba7a
hnmt2022 <chou0413bird@yahoo.co.jp>
2024-04-05 15:25:05 +0900

    パスの修正、不要なコメントの削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname               { Faker::Name.name }
     email                  { Faker::Internet.email }
-    password               { Faker::Internet.password(min_length: 6) }
+    password               { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation  { password }
     last_name              { '山田' }
     kana_last_name         { 'ヤマダ' }

cdd1a12617325150c7ea0a7c76a4ad8a27d826e9
hnmt2022 <chou0413bird@yahoo.co.jp>
2024-04-07 13:48:26 +0900

    ActiveStorageとActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,32 @@
 class Item < ApplicationRecord
+
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+  validates :price, presence: true
+  validates  :category_id, presence: true
+  validates  :salels_status_id, presence: true
+  validates  :shipping_fee_id, presence: true
+  validates  :item_prefecture_id, presence: true
+  validates  :sheduled_delivery_id, presence: true
+  
+
+  belongs_to :user
+
+  has_one_attached :image
+
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :salels_status
+  validates :salels_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :shipping_fee
+  validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :item_prefecture
+  validates :item_prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :sheduled_delivery
+  validates :sheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end

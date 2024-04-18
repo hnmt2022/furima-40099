94597e6050345371d742f9a3d751ad86c72b7611
hnmt2022 <chou0413bird@yahoo.co.jp>
2024-04-19 00:03:51 +0900

    テストコードの実装（住所）

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :order_address do
+    item_id                    {1}
+    #user_id                    {2}
+
+    postal_code                { '123-4567' }
+    prefecture_id              { Faker::Number.between(from: 2, to: 48) }
+    city                       { Faker::Lorem.sentence }
+    house_number               { Faker::Lorem.sentence }
+    building_name              { Faker::Lorem.sentence }
+    phone_number               { '09012345678' }
+
+
+    
+  end
+end

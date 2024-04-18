819cd608db59e281aba5370929f23d848f2f01d6
hnmt2022 <chou0413bird@yahoo.co.jp>
2024-04-18 22:57:17 +0900

    フォーム情報の保存

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,13 +12,12 @@ class OrderAddress
     validates :building_name
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
 
-    validates :order_id
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 
   def save
-    order = Order.create( item: item, user_id: user_id, item_id: item_id)
+    order = Order.create( user_id: user_id, item_id: item_id)
 
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
   end
 end
